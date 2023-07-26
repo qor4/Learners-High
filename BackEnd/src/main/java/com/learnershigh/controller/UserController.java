1df11acad6926b20b0d5ba5e94e31b66bd28a517
handaldog <handal@kakao.com>
2023-07-26 10:17:32 +0900

    [BE] Feat: 학위 경력 모두 출력 기능 수정

--- BackEnd/src/main/java/com/learnershigh/controller/UserController.java
+++ BackEnd/src/main/java/com/learnershigh/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
 
     // 유저 이메일로 userNo 값 뽑아내는 거 (userNo로 연관되니깐.)
     @GetMapping("/getUserNo")
-    public Long getUserNo(@RequestParam("userEmail") String userEmail){
+    public Long getUserNo(@RequestParam("userEmail") String userEmail) {
         return userService.getUserNo(userEmail);
     }
 
@@ -43,21 +43,29 @@ public class UserController {
         // false 면 가입 한 적 있음.
     }
 
-     // 회원탈퇴
+    // 회원탈퇴
     @PutMapping("/delete/{userNo}")
-    public Boolean deleteUser(@PathVariable("userNo") Long userNo){
-        return userService.userDelete(userNo);
+    public ResponseEntity<BaseResponseBody> deleteUser(@PathVariable("userNo") Long userNo) {
+        BaseResponseBody baseResponseBody = new BaseResponseBody("탈퇴되었습니다.");
+        try {
+            userService.userDelete(userNo);
+        } catch (Exception e) {
+            baseResponseBody.setResultCode(-1);
+            baseResponseBody.setResultMsg(e.getMessage());
+            return ResponseEntity.ok().body(baseResponseBody);
+        }
+        return ResponseEntity.ok().body(baseResponseBody);
+
     }
+
 
     // 비밀번호 변경 시 비밀번호 검사
     @GetMapping("/pwd-check/{userNo}")
-    public ResponseEntity<BaseResponseBody> pwdCheck(@PathVariable("userNo") Long userNo, @RequestParam("pwd") String pwd)
-    {
+    public ResponseEntity<BaseResponseBody> pwdCheck(@PathVariable("userNo") Long userNo, @RequestParam("pwd") String pwd) {
         BaseResponseBody baseResponseBody = new BaseResponseBody("비밀번호가 맞습니다.");
         try {
             userService.pwdCheck(userNo, pwd);
-        }
-        catch (IllegalStateException e){
+        } catch (IllegalStateException e) {
             baseResponseBody.setResultCode(-1);
             baseResponseBody.setResultMsg(e.getMessage());
             return ResponseEntity.ok().body(baseResponseBody);
@@ -81,7 +89,8 @@ public class UserController {
 
 
     }
-        // 아이디 중복
+
+    // 아이디 중복
     @GetMapping("/duplicate/id/{id}")
     public ResponseEntity<BaseResponseBody> duplicateId(@PathVariable("id") String userId) {
         BaseResponseBody baseResponseBody = new BaseResponseBody();
@@ -110,7 +119,7 @@ public class UserController {
     // 이메일 인증 번호
     @PostMapping("/cert/email")
     @ResponseBody
-    public String mailSend(@RequestParam String email) throws Exception{
+    public String mailSend(@RequestParam String email) throws Exception {
         String code = emailService.sendSimpleMessage(email);
         System.out.println("인증코드 : " + code);
         return code;
@@ -127,7 +136,7 @@ public class UserController {
         System.out.println("들어왔니");
 
         // 가입 한 적이 없다면.
-        if(userService.kakaoUserJoin(code) == null){
+        if (userService.kakaoUserJoin(code) == null) {
             return true;
         }
 
@@ -141,14 +150,14 @@ public class UserController {
     @PostMapping("/join/job/{userNo}")
     public void jobJoin(@RequestBody JobDto jobDto, @PathVariable("userNo") Long userNo) {
 
-         userService.jobJoin(jobDto, userNo);
+        userService.jobJoin(jobDto, userNo);
     }
 
     // 학위 추가
     @PostMapping("/join/edu/{userNo}")
     public void eduJoin(@RequestBody EduDto eduDto, @PathVariable("userNo") Long userNo) {
 
-         userService.eduJoin(eduDto, userNo);
+        userService.eduJoin(eduDto, userNo);
     }
 
     // 로그인
@@ -164,7 +173,7 @@ public class UserController {
 
     // 마이페이지에서 보일 사용자 정보 추출하기
     @GetMapping("/mypage/{userNo}")
-    public JoinDto mypageUser(@PathVariable("userNo") Long userNo){
+    public JoinDto mypageUser(@PathVariable("userNo") Long userNo) {
 
         return userService.mypageUser(userNo);
 
@@ -186,62 +195,29 @@ public class UserController {
     }
 
 
-        // 강사 학위 all 출력
+    // 강사 학위 all 출력
     @GetMapping("edu-all-list/{userNo}")
-    public List<EduDto> eduList(@PathVariable("userNo") User userNo){
-        List<EduCareer> eduList = userService.eduList(userNo);
-
-        List<EduDto> eduDtoList = new ArrayList<>();
-
-        for (EduCareer ec : eduList){
-            EduDto eduDto = new EduDto();
-
-            eduDto.setEduCareerNo(ec.getEduCareerNo());
-            eduDto.setUniversityName(ec.getUniversityName());
-            eduDto.setMajorName(ec.getMajorName());
-            eduDto.setDegree(ec.getDegree());
-            eduDto.setEduStartDate(ec.getEduStartDate());
-            eduDto.setEduEndDate(ec.getEduEndDate());
-
-            eduDtoList.add(eduDto);
-        }
-
-        return eduDtoList;
-
-
+    public List<EduDto> eduList(@PathVariable("userNo") User userNo) {
+        return userService.eduList(userNo);
     }
 
 
     // 강사 경력 all 출력
     @GetMapping("job-all-list/{userNo}")
-    public List<JobDto> jobList(@PathVariable("userNo") User userNo){
-        List<JobCareer> jobList = userService.jobList(userNo);
+    public List<JobDto> jobList(@PathVariable("userNo") User userNo) {
 
-        List<JobDto> jobDtoList = new ArrayList<>();
+       return userService.jobList(userNo);
 
-        for (JobCareer ec : jobList){
-            JobDto jobDto = new JobDto();
-
-           jobDto.setJobCareerNo(ec.getJobCareerNo());
-           jobDto.setCompanyName(ec.getCompanyName());
-           jobDto.setDepartName(ec.getDepartName());
-           jobDto.setHireStartDate(ec.getHireStartDate());
-           jobDto.setHireEndDate(ec.getHireEndDate());
-
-           jobDtoList.add(jobDto);
-
-        }
-
-        return jobDtoList;
     }
+
 
     // 강사 학력 수정
     @PutMapping("/modify/edu/{eduCareerNo}")
-    public ResponseEntity<BaseResponseBody> eduModify(@PathVariable("eduCareerNo") Long eduCareerNo, @RequestBody EduDto eduDto){
+    public ResponseEntity<BaseResponseBody> eduModify(@PathVariable("eduCareerNo") Long eduCareerNo, @RequestBody EduDto eduDto) {
         BaseResponseBody baseResponseBody = new BaseResponseBody("수정되었습니다.");
 
         try {
-            userService.eduModify(eduCareerNo,eduDto);
+            userService.eduModify(eduCareerNo, eduDto);
         } catch (Exception e) {
             baseResponseBody.setResultCode(-1);
             baseResponseBody.setResultMsg(e.getMessage());
@@ -253,7 +229,7 @@ public class UserController {
 
     // 강사 학력 삭제
     @DeleteMapping("/edu/delete/{eduCareerNo}")
-    public ResponseEntity<BaseResponseBody> eduDelete(@PathVariable("eduCareerNo") Long eduCareerNo){
+    public ResponseEntity<BaseResponseBody> eduDelete(@PathVariable("eduCareerNo") Long eduCareerNo) {
         BaseResponseBody baseResponseBody = new BaseResponseBody("삭제되었습니다.");
 
         try {
@@ -268,13 +244,23 @@ public class UserController {
 
     // 강사 경력 수정
     @PutMapping("/modify/job/{jobCareerNo}")
-    public void eduModify(@PathVariable("jobCareerNo") Long jobCareerNo, @RequestBody JobDto jobDto){
-    userService.jobModify(jobCareerNo, jobDto);
+    public ResponseEntity<BaseResponseBody> eduModify(@PathVariable("jobCareerNo") Long jobCareerNo, @RequestBody JobDto jobDto) {
+        BaseResponseBody baseResponseBody = new BaseResponseBody("수정되었습니다.");
+
+        try {
+            userService.jobModify(jobCareerNo, jobDto);
+        } catch (Exception e) {
+            baseResponseBody.setResultCode(-1);
+            baseResponseBody.setResultMsg(e.getMessage());
+            return ResponseEntity.ok().body(baseResponseBody);
+        }
+        return ResponseEntity.ok().body(baseResponseBody);
 
     }
+
     // 강사 경력 삭제
     @DeleteMapping("/job/delete/{jobCareerNo}")
-    public ResponseEntity<BaseResponseBody> jobDelete(@PathVariable("jobCareerNo") Long jobCareerNo){
+    public ResponseEntity<BaseResponseBody> jobDelete(@PathVariable("jobCareerNo") Long jobCareerNo) {
         BaseResponseBody baseResponseBody = new BaseResponseBody("삭제되었습니다.");
 
         try {
