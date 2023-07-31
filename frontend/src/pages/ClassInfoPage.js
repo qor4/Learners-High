01e56af09b55078feea255ca99403686eb7fec30
선다영 <seondy99@gmail.com>
2023-08-01 01:57:16 +0900

    [FE] Add: 강사 수업관리 페이지 + 강의 상세 페이지 컴포넌트 추가

---
+++ frontend/src/pages/ClassInfoPage.js
@@ -0,0 +1,7 @@
+// 강의 상세 페이지 (신청 페이지) url : /class/info/:강의no
+
+const ClassInfoPage = () => {
+    return;
+};
+
+export default ClassInfoPage;
---
+++ frontend/src/pages/EduTeacherManagePage.jsx
@@ -0,0 +1,7 @@
+// 강사 수업 관리 목록 페이지
+
+const EduTeacherManagePage = () => {
+    return;
+};
+
+export default EduTeacherManagePage;
