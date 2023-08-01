809c36938acf504828e800fb28d6c6e13a2cd081
임규돈 <mont4857@gmail.com>
2023-08-01 17:21:26 +0900

    [Docs] docker file 변경

--- start-prod.sh
+++ start-prod.sh
@@ -5,4 +5,11 @@ docker-compose -f docker-compose-prod.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-prod.yml up --build -d
 
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+result=$(docker images -f "dangling=true" -q)
+                    if [ -n "$result" ]
+                    then
+                        docker rmi -f $(docker images -f "dangling=true" -q)
+                    else
+                        echo "No such container images"
+                    fi
+
