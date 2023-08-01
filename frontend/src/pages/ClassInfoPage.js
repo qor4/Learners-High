7b335a64adbd7c1165fc807cb6523baada2d2338
선다영 <seondy99@gmail.com>
2023-08-02 00:25:10 +0900

    [FE] Feat: 강의 상세 페이지 데이터 가져오기 1차 완료

--- frontend/src/pages/ClassInfoPage.js
+++ frontend/src/pages/ClassInfoPage.js
@@ -4,6 +4,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { url } from "../api/APIPath";
 import { useParams } from "react-router-dom";
+import { HiOutlineHeart } from "react-icons/hi";
+
+import ClassInfoBox from "../components/class/ClassInfoBox";
+import Button from "../components/common/Button";
+import Card from "../components/common/Card";
+import TeacherIntroduceBox from "../components/class/TeacherIntroduceBox";
 
 const ClassInfoPage = () => {
     const { classNo } = useParams();
@@ -17,74 +23,57 @@ const ClassInfoPage = () => {
         });
     }, [classNo]);
 
-    // const classInfoDataSet = {
-    //     classInfo: {
-    //         classNo: 1,
-    //         userNo: 1,
-    //         userName: "신딩",
-    //         classTypeNo: 1,
-    //         classTypeName: "국어",
-    //         className: "수업 이름",
-    //         classStartDate: "2023-07-23",
-    //         classEndDate: "2023-07-22",
-    //         classInfo: "수업 정보",
-    //         maxStudent: 10,
-    //         totalStudent: 0,
-    //         classPrice: 11000,
-    //         classThumbnailImg: "",
-    //         classThumbnailInfo: "<html>",
-    //         classStatus: "강의 전",
-    //         classTotalRound: 0,
-    //     },
-    //     eduInfos: [],
-    //     jobInfos: [],
-    //     classRoundInfo: [
-    //         {
-    //             classRoundNo: 4,
-    //             classRoundNumber: 5,
-    //             classRoundTitle: "string",
-    //             classRoundStartDatetime: "2023-07-23T05:03:05",
-    //             classRoundEndDatetime: "2023-07-23T05:03:05",
-    //         },
-    //         {
-    //             classRoundNo: 5,
-    //             classRoundNumber: 4,
-    //             classRoundTitle: "string",
-    //             classRoundStartDatetime: "2023-07-30T05:03:05",
-    //             classRoundEndDatetime: "2023-07-30T05:03:05",
-    //         },
-    //         {
-    //             classRoundNo: 6,
-    //             classRoundNumber: 6,
-    //             classRoundTitle: "string",
-    //             classRoundStartDatetime: "2023-07-22T05:03:05",
-    //             classRoundEndDatetime: "2023-07-22T05:03:05",
-    //         },
-    //         {
-    //             classRoundNo: 7,
-    //             classRoundNumber: 7,
-    //             classRoundTitle: "string",
-    //             classRoundStartDatetime: "2023-07-24T05:03:05",
-    //             classRoundEndDatetime: "2023-07-24T05:03:05",
-    //         },
-    //     ],
-    // };
+    const classRoundInfo = classInfoDataSet.classRoundInfo;
 
     return (
         <div>
             <div>안녕하세요 {classNo}입니다.</div>
 
             {/* 강의 상세 정보 들어갈 공간 */}
-            <div>{classInfoDataSet.classInfo.userName}</div>
-            <div>{classInfoDataSet.classInfo.className}</div>
+            <ClassInfoBox classInfo={classInfoDataSet.classInfo} />
 
             {/* 강사 소개 */}
+            <h3>강사 소개</h3>
+            <TeacherIntroduceBox
+                classInfo={classInfoDataSet.classInfo}
+                eduInfo={classInfoDataSet.eduInfos}
+                jobInfo={classInfoDataSet.jobInfos}
+            />
 
             {/* 수업 소개 */}
+            <h3>수업 소개</h3>
+            {classInfoDataSet.classInfo && classInfoDataSet.classInfo.classInfo}
 
             {/* 회차 소개 => 추가할지 고민 수정@@@ */}
+            <h3>회차 소개</h3>
+            {classRoundInfo &&
+                classRoundInfo.map((round, index) => (
+                    <Card key={index}>
+                        <span>{round.classRoundTitle}</span>
+                        <span>
+                            {round.classRoundStartDatetime} ~{" "}
+                            {round.classRoundEndDatetime}
+                        </span>
+                    </Card>
+                ))}
 
             {/* 하단 고정 수강신청 바 */}
+            <div>
+                {classInfoDataSet.classInfo ? (
+                    <>
+                        <span>{classInfoDataSet.classInfo.className}</span>
+                        <span>{classInfoDataSet.classInfo.classPrice}원</span>
+                        <Button>
+                            수강 신청 ({" "}
+                            {classInfoDataSet.classInfo.totalStudent} /{" "}
+                            {classInfoDataSet.classInfo.maxStudent} 명 )
+                        </Button>
+                        <Button>
+                            <HiOutlineHeart />
+                        </Button>
+                    </>
+                ) : null}
+            </div>
         </div>
     );
 };
