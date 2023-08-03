3d4a1af1d6fd467f8451217a1ff664788766f989
선다영 <seondy99@gmail.com>
2023-08-03 22:40:37 +0900

    [FE] Feat: 학생 메인 페이지 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -2,8 +2,7 @@
 import { useSelector } from "react-redux";
 
 import GuestMain from "../components/main/GuestMain";
-import TeacherMain from "../components/main/TeacherMain";
-import StudentMain from "../components/main/StudentMain";
+import MemberMain from "../components/main/MemberMain";
 
 const MainPage = () => {
     const userType = useSelector((state) => state.user.userType);
@@ -14,11 +13,8 @@ const MainPage = () => {
             {/* 비로그인 메인페이지 */}
             {!userType && <GuestMain />}
 
-            {/* 강사 로그인 메인페이지 */}
-            {userType === "T" && <TeacherMain />}
-
-            {/* 학생 로그인 메인페이지 */}
-            {userType === "S" && <StudentMain />}
+            {/* 강사 로그인 메인페이지 / 학생 로그인 메인페이지 */}
+            {userType && <MemberMain />}
         </div>
     );
 };
