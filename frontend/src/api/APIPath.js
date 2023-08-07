cdac732d3a89dcba13858e50ff5b24b850063215
선다영 <seondy99@gmail.com>
2023-08-07 14:15:43 +0900

    [FE] Add: url 변경

--- frontend/src/api/APIPath.js
+++ frontend/src/api/APIPath.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 import dayjs from "dayjs";
 
-// export const url = "https://i9b105.p.ssafy.io:7777"
-export const url = "http://192.168.31.200:7777";
+export const url = "https://i9b105.p.ssafy.io:7777"
+// export const url = "http://192.168.31.200:7777";
 // const AUTH = "auth/"
 // const USER
 
