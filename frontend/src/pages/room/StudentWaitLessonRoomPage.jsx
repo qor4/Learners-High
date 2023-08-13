3baceb52f17cd9f003916ba11efe5e477f580d12
Min <dl_skflsalfm@naver.com>
2023-08-13 21:02:38 +0900

    [FE] Style: 학생 대기실, 학생 수업방 틀 완료

--- frontend/src/pages/room/StudentWaitLessonRoomPage.jsx
+++ frontend/src/pages/room/StudentWaitLessonRoomPage.jsx
@@ -333,7 +333,7 @@ const StudentWaitLessonRoomPage = () => {
                             }}
                         />
                             {videoEnabled && 
-                            <video 
+                            <Webcam 
                                 style={{
                                     position: "absolute",
                                     // objectFit: "cover",
