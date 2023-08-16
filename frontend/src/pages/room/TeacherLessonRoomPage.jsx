7279609c8553dbc45e4fde08d563c8a42c10609f
임규돈 <mont4857@gmail.com>
2023-08-17 02:05:35 +0900

    [FE] Fix: lessonroom 버그 수정

--- frontend/src/pages/room/TeacherLessonRoomPage.jsx
+++ frontend/src/pages/room/TeacherLessonRoomPage.jsx
@@ -710,7 +710,7 @@ const TeacherLessonRoomPage = () => {
                                                                         ),
                                                                     studentNo:
                                                                         Number(
-                                                                            sub.userNo
+                                                                            sub.studentNo
                                                                         ),
                                                                     teacherId:
                                                                         userId,
