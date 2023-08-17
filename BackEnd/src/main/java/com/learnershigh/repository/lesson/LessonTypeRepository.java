a4dc37dce385074a1895f35cbbd22ef6f91019ba
handaldog <handal@kakao.com>
2023-08-17 21:18:32 +0900

    [BE] Feat: 무슨 무슨 과목 듣는 지 봅는 기능

--- BackEnd/src/main/java/com/learnershigh/repository/lesson/LessonTypeRepository.java
+++ BackEnd/src/main/java/com/learnershigh/repository/lesson/LessonTypeRepository.java
@@ -2,10 +2,16 @@ package com.learnershigh.repository.lesson;
 
 
 import com.learnershigh.domain.lesson.LessonType;
+import com.learnershigh.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface LessonTypeRepository extends JpaRepository<LessonType, Integer> {
     LessonType findByLessonTypeNo(int lessonTypeNo);
+
+
+
 }
 
 
